7ab61260f2d4a6a2a7a8e4371c63b2e44e4b09ec
Douglas Sellers <dsellers@blurb.com>
2010-06-03 11:19:29 -0700

    Added back in the necessary pieces of the emacs starter kit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,3 +8,64 @@
 (require 'ido)
 (ido-mode t)
 (setq ido-enable-flex-matching t) ;; enable fuzzy matching(require 'ido'
+
+;; Install some starter kit stuff
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/jabber"))
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq package-user-dir (concat dotfiles-dir "elpa"))
+(setq custom-file (concat dotfiles-dir "custom.el"))
+
+;; These should be loaded on startup rather than autoloaded on demand
+;; since they are likely to be used in every session:
+
+(require 'cl)
+(require 'saveplace)
+(require 'ffap)
+(require 'uniquify)
+(require 'ansi-color)
+(require 'recentf)
+
+;; this must be loaded before ELPA since it bundles its own
+;; out-of-date js stuff. TODO: fix it to use ELPA dependencies
+;; TODO: this is causing issues with js2-mode; re-enable when fixed.
+(load "elpa-to-submit/nxhtml/autostart")
+
+;; Load up ELPA, the package manager:
+
+(require 'package)
+(package-initialize)
+(require 'starter-kit-elpa)
+
+;; Load up starter kit customizations:
+
+(require 'starter-kit-defuns)
+(require 'starter-kit-bindings)
+(require 'starter-kit-misc)
+(require 'starter-kit-registers)
+(require 'starter-kit-eshell)
+(require 'starter-kit-lisp)
+(require 'starter-kit-ruby)
+(require 'starter-kit-js)
+
+(regen-autoloads)
+(load custom-file 'noerror)
+
+;; More complicated packages that haven't made it into ELPA yet:
+
+(autoload 'jabber-connect "jabber" "" t)
+;; TODO: rinari, slime
+
+;; You can keep system- or user-specific customizations here:
+
+(setq system-specific-config (concat dotfiles-dir system-name ".el")
+      user-specific-config (concat dotfiles-dir user-login-name ".el"))
+
+(if (file-exists-p system-specific-config) (load system-specific-config))
+(if (file-exists-p user-specific-config) (load user-specific-config))
+
+
+;; init.el ends
